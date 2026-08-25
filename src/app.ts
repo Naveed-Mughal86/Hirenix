ee088798918390e476dcb12546de4a7f2b4f784d
Naveed-Mughal86 <naveedzulfiqar100@gmail.com>
2026-08-25 13:17:38 +0500

    updated app.ts and server.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,17 @@
+
 import fastify from "fastify";
 
 export function buildApp(){
 const app = fastify();
+
+// Infrastructure ----------------------
+app.get('/health', async(_request, reply) => {
+    return reply.send({
+        status: 'ok',
+        uptime: Math.floor(process.uptime()),
+        timestamp: new Date().toISOString(),
+    });
+});
 
 app.get('/', async(request, reply) => {
     return {status: 'ok'}
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
+
 import { buildApp } from "./app";
+import { config } from './shared/config';
 
 const app = buildApp();
 
-const PORT = 3000;
+// const PORT = 3000;
 
-app.listen({port: PORT}, () => {
-    console.log(`Server running on http://localhost:${PORT}`)
+app.listen({port: config.PORT}, () => {
+    console.log(`Server running on http://localhost:${config.PORT}`)
 })
